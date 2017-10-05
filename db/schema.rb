8d499dfbd27d311d0830ca75c4c647017193369a
Marko <nikolina.sekulic@gmail.com>
2017-10-05 14:02:11 +0000

    Add Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005100121) do
+ActiveRecord::Schema.define(version: 20171005133236) do
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
